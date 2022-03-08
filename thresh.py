7469aff62d344dafb1d1a30bc91e164702e2a385
Simon Pettersson <simon.pettersson@protonmail.com>
2022-03-08 13:24:44 +0100

    Thresholding/Binarization

---
+++ thresh.py
@@ -0,0 +1,23 @@
+from ctypes import cast
+import cv2 as cv
+
+img = cv.imread("Photos/cats.jpg")
+cv.imshow("Cats", img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Gray", gray)
+
+# Simple thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow("Simple Thresholding", thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow("Simple Thresholding Inverse", thresh_inv)
+
+# Adaptive thresholding
+adaptive_thresh = cv.adaptiveThreshold(
+    gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 13, 9
+)
+cv.imshow("Adaptive Thresholding", adaptive_thresh)
+
+cv.waitKey(0)
