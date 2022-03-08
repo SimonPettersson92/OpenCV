7485a358ab8ac3b165cbe3cc1a584c55ff42cba3
Simon Pettersson <simon.pettersson@protonmail.com>
2022-03-08 11:38:24 +0100

    Histogram computation

---
+++ histogram.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv.imread("Photos/cats.jpg")
+cv.imshow("Cats", img)
+
+blank = np.zeros(img.shape[:2], dtype="uint8")
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Gray", gray)
+
+mask = cv.circle(blank, (img.shape[1] // 2, img.shape[0] // 2), 100, 255, -1)
+
+masked_image = cv.bitwise_and(img, img, mask=mask)
+cv.imshow("Mask", masked_image)
+
+# Grayscale histogram
+# gray_hist = cv.calcHist([gray], [0], mask, [256], [0, 256])
+
+# plt.figure()
+# plt.title("Grayscale Histogram")
+# plt.xlabel("Bins")
+# plt.ylabel("Number of pixels")
+# plt.plot(gray_hist)
+# plt.xlim([0, 256])
+
+# plt.show()
+
+# Colour Histogram
+plt.figure()
+plt.title("Colour Histogram")
+plt.xlabel("Bins")
+plt.ylabel("Number of pixels")
+
+colours = ("b", "g", "r")
+for i, col in enumerate(colours):
+    hist = cv.calcHist([img], [i], mask, [256], [0, 256])
+    plt.plot(hist, color=col)
+    plt.xlim([0, 256])
+
+plt.show()
+
+cv.waitKey(0)
